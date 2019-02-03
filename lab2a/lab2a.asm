5895e2d93b50b495c91e8f8f0ed674e5ff27a064
Mesa Perry <mesagp2@illinois.edu>
2019-02-03 00:40:06 -0600

    completed lab2a

--- lab2a/lab2a.asm
+++ lab2a/lab2a.asm
@@ -10,7 +10,48 @@ CHAR_RETURN     .FILL x000D
 ;if ( push onto stack if ) pop from stack and check if popped value is (
 ;input - R0 holds the input
 ;output - R5 set to -1 if unbalanced. else 1.
+
+LOOP	IN						;
+		JSR IS_BALANCED			;
+		BR LOOP
+
+
+
 IS_BALANCED
+        ST R1, SaveR1      		;save R1
+		ADD R1, R0, #-10		;
+		BRz COMPLETE			;
+		ADD R1, R1, #-16		;
+		ADD R1, R1, #-6			;
+		BRz SPC_DET				;
+		ADD	R1, R1, #-8			;
+		BRz PUSH				;
+		LD R1, STACK_TOP		;
+		ADD R1, R1, #1			;
+		LDR R1, R1, #0			;
+		ADD R1, R1, #-15		;
+		ADD R1, R1, #-15		;
+		ADD R1, R1, #-10		;
+		BRz POP					;
+		AND R5, R5, #0			;
+		ADD R5, R5, #-1			;
+		HALT					;
+
+
+SPC_DET	RET						;
+
+COMPLETE
+		LD R3, STACK_START		;
+		LD R4, STACK_TOP		;
+		AND R5, R5, #0			;
+		ADD R5, R5, #-1			;
+		NOT R3, R3				;
+		ADD R3, R3, #1			;
+		ADD R3, R3, R4			;
+		BRnp DONE				;
+		ADD R5, R5, #2			;
+DONE	HALT					;
+		
 
 NEG_OPEN .FILL xFFD8
 ;IN:R0, OUT:R5 (0-success, 1-fail/overflow)
@@ -21,7 +62,7 @@ PUSH
         ST R4, PUSH_SaveR4      ;save R4
         AND R5, R5, #0          ;
         LD R3, STACK_END        ;
-        LD R4, STACk_TOP        ;
+        LD R4, STACK_TOP        ;
         ADD R3, R3, #-1         ;
         NOT R3, R3              ;
         ADD R3, R3, #1          ;
@@ -36,6 +77,7 @@ OVERFLOW
 DONE_PUSH
         LD R3, PUSH_SaveR3      ;
         LD R4, PUSH_SaveR4      ;
+        LD R1, SaveR1   	    ;
         RET
 
 
@@ -65,6 +107,7 @@ UNDERFLOW
 DONE_POP
         LD R3, POP_SaveR3       ;
         LD R4, POP_SaveR4       ;
+        LD R1, SaveR1   	    ;
         RET
 
 
@@ -73,6 +116,11 @@ POP_SaveR4      .BLKW #1        ;
 STACK_END       .FILL x3FF0     ;
 STACK_START     .FILL x4000     ;
 STACK_TOP       .FILL x4000     ;
+
+KBSRPtr			.FILL xFE00		;
+KBDRPtr			.FILL xFE02		;
+SaveR1     		.BLKW #1        ;
+
 
 .END
 
