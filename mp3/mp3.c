98066d473652b7db1e49dadd48c3c459c84bc1f6
Mesa Perry <mesagp2@illinois.edu>
2019-02-12 22:09:26 -0600

    complete MP3

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,30 @@
+/* This program was written by Mesa Perry. The goal is
+to print a specific line of Pascal's Triangle. It
+prompts the user to enter a row index, then prints
+the line corresponding to the inputted row index.
+This program is intended to work for rows 40 or less */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  int row;
-
-  printf("Enter a row index: ");
-  scanf("%d",&row);
-
-  // Write your code here
-
-  return 0;
+    int n; /* initialize targeted row index */
+    int k; /* initialize targeted coefficient */
+    printf("Enter the row index : ");
+    scanf("%u", &n);
+    for(k = 0; k <= n; k ++) /* loop through all coefficients in the line */
+    {
+        int i;
+        unsigned long cof; /* total of all products */
+        cof = 1;
+        for(i = 1; i <= k; i ++) /* loop through multiplications required to calculate coeffcient */
+        {
+            cof *= (n + 1 - i);
+            cof /= i;
+        }
+        printf("%lu ", cof); /* print coefficient */
+    }
+    return 0;
 }
+
